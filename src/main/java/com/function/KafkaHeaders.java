5eac29bc2d247e644227aca02a62b47fe90ed77e
David Barkol <dabarkol@microsoft.com>
2022-05-04 00:02:10 -0700

    initial revision

---
+++ src/main/java/com/function/KafkaHeaders.java
@@ -0,0 +1,12 @@
+package com.function;
+
+public class KafkaHeaders{
+    String Key;
+    String Value;
+
+    public KafkaHeaders(String key, String value) {
+        this.Key = key;
+        this.Value = value;
+    }
+
+}
